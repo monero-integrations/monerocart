dd783c706991dc5349229872d45e5bffbee16097
serhack <27734319+serhack@users.noreply.github.com>
2018-01-19 14:42:47 +0100

    Create monero.php

---
+++ admin/language/en-gb/extension/payment/monero.php
@@ -0,0 +1,32 @@
+<?php
+
+//Payment page links
+$_['text_monero'] = '<a href="http://monerointegrations.com">The Monero Integrations Project</a>';
+
+// Heading
+$_['heading_title']					= 'Monero Payment Gateway';
+
+// Settings
+$_['wallet_address']				= 'Monero Wallet address';
+
+$_['wallet_host']					= 'Monero RPC Host';
+$_['wallet_port']					= 'Monero RPC Port';
+$_['wallet_ssl']					= 'SSL';
+$_['monero_yes']						= 'Yes';
+$_['monero_no']						= 'No';
+$_['monero_enable']					= 'Enable';
+$_['monero_disable']					= 'Disable';
+$_['monero_order_status']			= 'Initial order status';
+$_['monero_geo_zone']				= 'Geographical zone';
+$_['monero_status']					= 'Status';
+$_['monero_sort_order']				= 'Sort Order';
+
+// Text
+$_['text_payment']					= 'Payment';
+$_['text_success']					= 'Success: You have modified Monero payment gateway!';
+$_['text_extension']     			= 'Extensions';
+
+// Error
+$_['error_permission']				= 'Warning: You do not have permission to modify Monero payment gateway!';
+
+?>
