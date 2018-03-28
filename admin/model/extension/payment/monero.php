8565cc2ea1d63a8ff2a3b736547e7ed11727982e
serhack <27734319+serhack@users.noreply.github.com>
2018-03-28 15:08:50 +0200

    Update monero.php

--- admin/model/extension/payment/monero.php
+++ admin/model/extension/payment/monero.php
@@ -1,21 +1,19 @@
 <?php
-
-class ModelExtensionPaymentMonero extends Model {
-
-  public function install(){
-    $this->db->query("
-                       CREATE TABLE `" . DB_PREFIX . "order_monero` (
-                         `order_id` int(11) NOT NULL,
-                         `payment_id` varchar(255) NOT NULL,
-                         PRIMARY KEY `order_id` (`order_id`)
-                       ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-                   ");
-  }
-
-  public function uninstall(){
-    $this->db->query("DROP TABLE IF EXISTS `" . DB_PREFIX . "order_monero`;");
-  }
-
+class ModelExtensionPaymentMonero extends Model
+{
+    public function createDatabaseTables()
+    {
+        $sql = "CREATE TABLE IF NOT EXISTS `" . DB_PREFIX . "monero_opencart` (
+            `bind_id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            `order_id` int(32) NOT NULL,
+            `session_id` varchar(32) NOT NULL,
+            `status` varchar(32)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+        $this->db->query($sql);
+    }
+    public function dropDatabaseTables()
+    {
+        $sql = "DROP TABLE IF EXISTS `" . DB_PREFIX . "monero_opencart`;";
+        $this->db->query($sql);
+    }
 }
-
-?>
